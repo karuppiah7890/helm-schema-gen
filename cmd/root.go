f97e83183200ae8f9272323db5c97f96ac002760
cw-ozaki <log@logn.in>
2021-02-11 15:28:39 +0900

    support YAML format comment

--- cmd/root.go
+++ cmd/root.go
@@ -2,13 +2,63 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-
 	"github.com/karuppiah7890/go-jsonschema-generator"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"os"
+	"strings"
 )
+
+func commentOutYAML(node *yaml.Node) *yaml.Node {
+	switch node.Kind {
+	case yaml.DocumentNode, yaml.SequenceNode:
+		for i, child := range node.Content {
+			node.Content[i] = commentOutYAML(child)
+		}
+		return node
+	case yaml.MappingNode:
+		for i := 0; i < len(node.Content); i = i + 2 {
+			key := node.Content[i]
+			value := node.Content[i+1]
+			switch value.Kind {
+			case yaml.SequenceNode:
+				if len(key.FootComment) == 0 || len(value.Content) != 0 {
+					node.Content[i+1] = commentOutYAML(value)
+					continue
+				}
+				comment := strings.ReplaceAll(key.FootComment, "#", strings.Repeat(" ", key.Column-1))
+				root := yaml.Node{}
+				if err := yaml.Unmarshal([]byte(comment), &root); err != nil {
+					continue
+				}
+				if root.Content[0].Kind != value.Kind {
+					continue
+				}
+				node.Content[i+1] = root.Content[0]
+			case yaml.MappingNode:
+				if len(key.FootComment) == 0 || len(value.Content) != 0 {
+					node.Content[i+1] = commentOutYAML(value)
+					continue
+				}
+				comment := strings.ReplaceAll(key.FootComment, "#", strings.Repeat(" ", key.Column-1))
+				root := yaml.Node{}
+				if err := yaml.Unmarshal([]byte(comment), &root); err != nil {
+					continue
+				}
+				if root.Content[0].Kind != yaml.MappingNode {
+					continue
+				}
+				node.Content[i+1] = root.Content[0]
+			default:
+				node.Content[i+1] = commentOutYAML(value)
+			}
+		}
+		return node
+	default:
+		return node
+	}
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -35,7 +85,18 @@ Examples:
 		if err != nil {
 			return fmt.Errorf("error when reading file '%s': %v", valuesFilePath, err)
 		}
-		err = yaml.Unmarshal(valuesFileData, &values)
+		root := yaml.Node{}
+		err = yaml.Unmarshal(valuesFileData, &root)
+		if err != nil {
+			return fmt.Errorf("error when unmarshaling file '%s': %v", valuesFilePath, err)
+		}
+		if ok, _ := cmd.Flags().GetBool("yaml-comment"); ok {
+			root = *commentOutYAML(&root)
+		}
+		err = root.Decode(&values)
+		if err != nil {
+			fmt.Println(err)
+		}
 		s := &jsonschema.Document{}
 		s.ReadDeep(&values)
 		fmt.Println(s)
@@ -46,6 +107,7 @@ Examples:
 
 // Execute executes the root command
 func Execute() {
+	rootCmd.Flags().Bool("yaml-comment", false, "Generate a schema with YAML format comments as YAML when it is an empty array or map")
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -6,7 +6,5 @@ require (
 	github.com/karuppiah7890/go-jsonschema-generator v0.0.0-20191229070329-082ad3307d9e
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5 // indirect
-	gopkg.in/yaml.v2 v2.2.7
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
-
-replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.7
